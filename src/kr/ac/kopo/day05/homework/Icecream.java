14c13bb8ee1b1489e9e4b88b5d999bb26d8b14a2
Yura Park <yrpr1014@gmail.com>
2022-03-15 21:44:47 +0900

    homework done

--- src/kr/ac/kopo/day05/homework/Icecream.java
+++ src/kr/ac/kopo/day05/homework/Icecream.java
@@ -8,43 +8,39 @@ public class Icecream {
 	
 	public static void main(String[] args) {
 		
-		Icecream ic = new Icecream();
 		Scanner sc = new Scanner(System.in);
-		int sum = 0;
 		
 		System.out.println("아이스크림 몇 개 구매할꺼야?");
 		int num = sc.nextInt();
-		
+		int sum = 0;
+		Icecream[] ice = new Icecream[num];
+				
 		for(int i = 0 ; i < num ; i++) {
 			System.out.printf("*** %d번째 아이스크림 구매정보 ***\n", i+1);
-		
-			System.out.println("아이스크림명 : ");
-			ic.name = sc.next();
+			ice[i] = new Icecream();
+			
+			System.out.print("아이스크림명 : ");
+			ice[i].name = sc.next();
 			
 			System.out.println("아이스크림가격 : ");
-			ic.price = sc.nextInt();
+			ice[i].price = sc.nextInt();
 			
+			sum += ice[i].price;
 			System.out.println();
 		}
-		
-		Icecream[] list = new Icecream[num];
+				
 		System.out.printf("< 총 %d개의 아이스크림 구매정보 출력 >\n", num);
 		System.out.println("번호\t아이스크림명\t가격");
 		
-		for(int i = 0; i < list.length ; i++) {
-			System.out.printf("%d\t%s\t%d", (i+1), list[i].name, list[i].price);
-			
-			sum += (list[i].price);
-			System.out.println("총 판매액 : " + sum + "원");
-			
+		for(int j = 0; j < ice.length ; j++) {
+			System.out.printf("%d\t%s\t\t%d\n", (j+1), ice[j].name, ice[j].price);
+				
 		}
-		
 		System.out.println();
-				 
+		System.out.println("총 판매액 : " + sum + "원");				 
 			
 	}
 
-
-	}
+}
 
 
